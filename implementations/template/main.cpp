5f882d4ace6d64516d9f8f45ea0160d2228c0636
ncduy <caoduy73@gmail.com>
2022-04-02 16:14:50 +0700

    Add main templates

---
+++ implementations/template/main.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define print_op(...) ostream& operator<<(ostream& out, const __VA_ARGS__& u)
+template<typename A, typename B> print_op(pair<A, B>) { return out << "(" << u.first << ", " << u.second << ")"; }
+template<typename T_container, typename T = typename enable_if<!is_same<T_container, string>::value, typename T_container::value_type>::type> print_op(T_container) { out << "{"; string sep; for (const T &x : u) out << sep << x, sep = ", "; return out << "}"; }
+template<typename T> void dbg_out(string s, T x) {cerr << s << " = " << x << "\n";}
+template<typename T, typename... Args> void dbg_out(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
+if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << s.substr(0, i) << " = " << x << " | "; dbg_out(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
+#ifdef LOCAL
+#define dbg(...) dbg_out(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+using ll = long long;
+using ld = long double;
+#define ar array
+#define sz(x) ((int)x.size())
+#define rep(i, a, b) for (int i = (int)(a); i < (int)(b); i++) 
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const int MAX_L = 20;
+const int MAX_C = 26;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+
+
+void solve(int tc = 0) {
+    
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve(t);
+    }
+}
